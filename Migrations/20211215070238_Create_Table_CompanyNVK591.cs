20305f23340aa99f054107b64e2f76a805629d74
khoatngguyen <nguyenvankkhoat2000@gmail.com>
2021-12-15 14:03:30 +0700

    Final_Commit_NVK_591

---
+++ Migrations/20211215070238_Create_Table_CompanyNVK591.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace nguyenvankhoat591.Migrations
+{
+    public partial class Create_Table_CompanyNVK591 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CompanyNVK591",
+                columns: table => new
+                {
+                    CompanyId = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    CompanyName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyNVK591", x => x.CompanyId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NVK0591",
+                columns: table => new
+                {
+                    NVKId = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    NVKName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    NVKGender = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NVK0591", x => x.NVKId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanyNVK591");
+
+            migrationBuilder.DropTable(
+                name: "NVK0591");
+        }
+    }
+}
---
+++ Migrations/20211215070258_Create_Table_NVK591.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace nguyenvankhoat591.Migrations
+{
+    public partial class Create_Table_NVK591 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

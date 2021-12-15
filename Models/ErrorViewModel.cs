c1fca6e72425ada377c996f36b0d5b918b40a4ac
khoatngguyen <nguyenvankkhoat2000@gmail.com>
2021-12-15 13:04:22 +0700

    first_commit_nvk591

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace nguyenvankhoat591.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
